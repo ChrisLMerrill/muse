ecfd5f424a5d543d5e3723e948719f0fc33efbb8
Chris <ChrisLMerrill@users.noreply.github.com>
2017-09-17 18:11:31 -0400

    Improve docs on MuseResource.

--- core/src/main/java/org/musetest/core/MuseResource.java
+++ core/src/main/java/org/musetest/core/MuseResource.java
@@ -6,7 +6,9 @@ import org.musetest.core.resource.*;
 import java.io.*;
 
 /**
- * A MuseResource is the reference for any artifact that can exist at the top level of a project.
+ * A MuseResource is the reference for any first-class artifact that can exist in a MuseProject.
+ * Note that only artifacts that can be stored independently are considered a MuseResource. Serializable
+ * widgets that must be contained by something else are not a MuseResource.
  *
  * @author Christopher L Merrill (see LICENSE.txt for license details)
  */
