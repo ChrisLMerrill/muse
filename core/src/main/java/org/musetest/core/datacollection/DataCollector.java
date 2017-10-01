643d66108191ea6d4ed6f209dd9acefa8eac01a8
Chris <ChrisLMerrill@users.noreply.github.com>
2017-10-01 14:24:08 -0400

    Add DataCollector interface.

---
+++ core/src/main/java/org/musetest/core/datacollection/DataCollector.java
@@ -0,0 +1,12 @@
+package org.musetest.core.datacollection;
+
+import org.musetest.core.context.*;
+
+/**
+ * @author Christopher L Merrill (see LICENSE.txt for license details)
+ */
+public interface DataCollector extends ContextInitializer
+	{
+	}
+
+
