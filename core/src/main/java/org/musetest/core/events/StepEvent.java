94bebf2131b38b0cbecc078cc102fef9aaf065bf
Chris <ChrisLMerrill@users.noreply.github.com>
2017-10-26 16:11:55 -0400

    Collect the step id in StepEvent (for use by listeners, data collection, etc)

--- core/src/main/java/org/musetest/core/events/StepEvent.java
+++ core/src/main/java/org/musetest/core/events/StepEvent.java
@@ -12,14 +12,16 @@ public class StepEvent extends MuseEvent
     public StepEvent(MuseEventType type, StepConfiguration config, StepExecutionContext context)
         {
         super(type);
-        _config = config;
+        if (config.getMetadataField(StepConfiguration.META_ID) != null)
+	        _stepid = config.getMetadataField(StepConfiguration.META_ID).toString();
         _step_description = context.getProject().getStepDescriptors().get(config).getShortDescription(config);
         }
 
     public StepEvent(MuseEventType type, StepConfiguration config, StepExecutionContext context, StepExecutionResult result)
         {
         super(type);
-        _config = config;
+        if (config.getMetadataField(StepConfiguration.META_ID) != null)
+	        _stepid = config.getMetadataField(StepConfiguration.META_ID).toString();
         _step_description = context.getProject().getStepDescriptors().get(config).getShortDescription(config);
         _result = result;
         }
@@ -47,9 +49,9 @@ public class StepEvent extends MuseEvent
         return builder.toString();
         }
 
-    public StepConfiguration getConfig()
+    public String getStepId()
         {
-        return _config;
+        return _stepid;
         }
 
     public StepExecutionResult getResult()
@@ -59,5 +61,5 @@ public class StepEvent extends MuseEvent
 
     private String _step_description;
     private StepExecutionResult _result;
-    private StepConfiguration _config;
+    private String _stepid;
     }
