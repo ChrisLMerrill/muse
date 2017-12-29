bb66a2018829540744b801f223ab34dcf931cb05
Chris <ChrisLMerrill@users.noreply.github.com>
2017-12-29 11:39:00 -0500

    minor internal change to GenericResourceConfiguration

--- core/src/main/java/org/musetest/core/resource/generic/GenericResourceConfiguration.java
+++ core/src/main/java/org/musetest/core/resource/generic/GenericResourceConfiguration.java
@@ -7,8 +7,8 @@ import java.util.*;
 
 /**
  * A MuseResource that uses a general set of configuration parameters. It is intended for resources which
- * have a few basic configurations for the user to supply. When used in conjucntion with with the supported
- * annotations, the IDE can generate an editor for those configuration settings without needing custom UI code.
+ * have a few basic configurations for the user to supply. When used in conjunction with with the supported
+ * annotations, the IDE can generate an editor for those configuration settings  without needing custom UI code.
  *
  * @author Christopher L Merrill (see LICENSE.txt for license details)
  */
@@ -18,20 +18,20 @@ public abstract class GenericResourceConfiguration extends BaseMuseResource
 	 * Used for JSON serialization. Use parameters() to get access to the sub-sources
 	 * for this configuration.
 	 */
-	public Map<String, ValueSourceConfiguration> getSources()
+	public Map<String, ValueSourceConfiguration> getParameters()
 		{
-		return _sources.getSourceMap();
+		return _parameters.getSourceMap();
 		}
 
-	public void setSources(Map<String, ValueSourceConfiguration> sources)
+	public void setParameters(Map<String, ValueSourceConfiguration> sources)
 		{
-		_sources.setSourceMap(sources);
+		_parameters.setSourceMap(sources);
 		}
 
 	public NamedSourcesContainer parameters()
 		{
-		return _sources;
+		return _parameters;
 		}
 
-	private NamedSourcesContainer _sources = new NamedSourcesContainer();
+	private NamedSourcesContainer _parameters = new NamedSourcesContainer();
 	}
