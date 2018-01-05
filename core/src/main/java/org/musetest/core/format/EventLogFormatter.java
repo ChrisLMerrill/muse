31e781c3127c111e21d5f3732f121e2b5f6acc72
Chris <ChrisLMerrill@users.noreply.github.com>
2018-01-05 14:52:49 -0500

    Implement Reformatter support for rewriting EventLog.json as text.

---
+++ core/src/main/java/org/musetest/core/format/EventLogFormatter.java
@@ -0,0 +1,69 @@
+package org.musetest.core.format;
+
+import org.musetest.core.*;
+import org.musetest.core.events.*;
+import org.musetest.core.resource.json.*;
+import org.musetest.core.util.*;
+import org.slf4j.*;
+
+import java.io.*;
+
+/**
+ * @author Christopher L Merrill (see LICENSE.txt for license details)
+ */
+public class EventLogFormatter implements Reformatter
+	{
+	@Override
+	public boolean canReformat(MuseProject project, Object object, String format)
+		{
+		return object instanceof EventLog && format.equals("txt");
+		}
+
+	@Override
+	public boolean canReformat(MuseProject project, File file, String type, String format)
+		{
+		return type.equals("EventLog") && format.equals("txt");
+		}
+
+	@Override
+	public void reformat(MuseProject project, Object object, String format, OutputStream outstream)
+		{
+		}
+
+	@Override
+	public void reformat(MuseProject project, File file, String type, String format, OutputStream outstream)
+		{
+		try (FileInputStream instream = new FileInputStream(file))
+			{
+			EventLog log = JsonResourceSerializer.getMapper(new TypeLocator(project.getClassLocator())).readValue(instream, EventLog.class);
+			write(project, log, outstream);
+			}
+		catch (IOException e)
+			{
+			LOG.error("Failed to read EventLog", e);
+			}
+		}
+
+	private void write(MuseProject project, EventLog log, OutputStream outstream)
+		{
+		try
+			{
+			PrintStream printstream;
+			if (outstream instanceof PrintStream)
+				printstream = (PrintStream) outstream;
+			else
+				printstream = new PrintStream(outstream);
+			EventLogPrinter printer = new EventLogPrinter(printstream);
+			for (MuseEvent event : log.getEvents())
+				printer.print(event);
+			}
+		catch (Exception e)
+			{
+			LOG.error("Failed to write EventLog", e);
+			}
+		}
+
+	private final static Logger LOG = LoggerFactory.getLogger(EventLogFormatter.class);
+	}
+
+
---
+++ core/src/main/java/org/musetest/core/format/Reformatters.java
@@ -0,0 +1,93 @@
+package org.musetest.core.format;
+
+import org.musetest.core.*;
+import org.slf4j.*;
+
+import java.io.*;
+import java.lang.reflect.*;
+import java.util.*;
+
+/**
+ * @author Christopher L Merrill (see LICENSE.txt for license details)
+ */
+public class Reformatters
+	{
+	public static Reformatters get(MuseProject project)
+		{
+		Reformatters reformatters = REFORMATTERS.get(project);
+		if (reformatters == null)
+			{
+			reformatters = new Reformatters(project);
+			REFORMATTERS.put(project, reformatters);
+			}
+		return reformatters;
+		}
+
+	private Reformatters(MuseProject project)
+		{
+		_project = project;
+		List<Class> implementors = project.getClassLocator().getImplementors(Reformatter.class);
+		for (Class the_class : implementors)
+			{
+			if (!Modifier.isAbstract(the_class.getModifiers()))
+				{
+				try
+					{
+					the_class.newInstance();  // we can successfully instantiate without an exception
+					_classes.add(the_class);
+					}
+				catch (Exception e)
+					{
+					LOG.error(String.format("Unable to instantiate a %s. Does it have a public no-args constructor?", the_class.getSimpleName()));
+					// ignore this one
+					}
+				}
+			}
+
+		}
+
+	public List<Reformatter> find(Object object, String format_type)
+		{
+		List<Reformatter> reformatters = new ArrayList<>();
+		for (Class implementor : _classes)
+			{
+			try
+				{
+				Reformatter reformatter = (Reformatter) implementor.newInstance();  // we can successfully instantiate without an exception
+				if (reformatter.canReformat(_project, object, format_type))
+					reformatters.add(reformatter);
+				}
+			catch (Exception e)
+				{
+				LOG.error("Unexpected failure. Why are we unable to instantiate it here?", e);  // should have caught in constructor!
+				}
+			}
+		return reformatters;
+		}
+
+	public List<Reformatter> find(File file, String input_type, String format_type)
+		{
+		List<Reformatter> reformatters = new ArrayList<>();
+		for (Class implementor : _classes)
+			{
+			try
+				{
+				Reformatter reformatter = (Reformatter) implementor.newInstance();  // we can successfully instantiate without an exception
+				if (reformatter.canReformat(_project, file, input_type, format_type))
+					reformatters.add(reformatter);
+				}
+			catch (Exception e)
+				{
+				LOG.error("Unexpected failure. Why are we unable to instantiate it here?", e);  // should have caught in constructor!
+				}
+			}
+		return reformatters;
+		}
+
+	private final MuseProject _project;
+	private Set<Class> _classes = new HashSet<>();
+
+	private final static Map<MuseProject, Reformatters> REFORMATTERS = new HashMap<>();
+
+	private final static Logger LOG = LoggerFactory.getLogger(Reformatters.class);
+	}
