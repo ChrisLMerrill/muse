94574a2557b42d3d122097058303cbca18e78efb
Chris <ChrisLMerrill@users.noreply.github.com>
2018-01-16 21:33:20 -0500

    Enhance CompoundPlugin to accept the list of plugin ids as a single string, a list of strings or a comma-separated list in a string.

--- core/src/main/java/org/musetest/core/test/plugins/CompoundPlugin.java
+++ core/src/main/java/org/musetest/core/test/plugins/CompoundPlugin.java
@@ -5,6 +5,8 @@ import org.musetest.core.resource.*;
 import org.musetest.core.test.plugin.*;
 import org.musetest.core.values.*;
 
+import java.util.*;
+
 /**
  * @author Christopher L Merrill (see LICENSE.txt for license details)
  */
@@ -25,8 +27,25 @@ public class CompoundPlugin extends BaseTestPlugin
 			return false;
 
 		MuseValueSource source = BaseValueSource.getValueSource(_configuration.parameters(), CompoundPluginConfiguration.LISTS_PARAM, true, context.getProject());
-		String id_list_param = BaseValueSource.getValue(source, context, false, String.class);
-		String[] id_list = id_list_param.split(",");
+		Object id_list_obj = BaseValueSource.getValue(source, context, false);
+		String[] id_list;
+		if (id_list_obj instanceof List)
+			{
+			final List<String> list = (List) id_list_obj;
+			id_list = new String[list.size()];
+			for (int i = 0; i < list.size(); i++)
+				id_list[i] = ((List) id_list_obj).get(i).toString();
+			}
+		else if (id_list_obj.toString().contains(","))
+			{
+			String id_list_param = BaseValueSource.getValue(source, context, false, String.class);
+			id_list = id_list_param.split(",");
+			}
+		else
+			{
+			id_list = new String[1];
+			id_list[0] = id_list_obj.toString();
+			}
 
 		for (String id : id_list)
 			{
