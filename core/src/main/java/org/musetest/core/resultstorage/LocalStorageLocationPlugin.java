cea8fa89b69fbaa5bc01d54c1be0221fba2bafa1
chris <ChrisLMerrill@users.noreply.github.com>
2018-06-25 16:27:00 -0400

    Raise message event with the local storage location.

--- core/src/main/java/org/musetest/core/resultstorage/LocalStorageLocationPlugin.java
+++ core/src/main/java/org/musetest/core/resultstorage/LocalStorageLocationPlugin.java
@@ -33,7 +33,14 @@ public class LocalStorageLocationPlugin extends GenericConfigurableTestPlugin im
 		_output_folder = new File(output_folder_path);
 		if (!_output_folder.exists())
 			if (!_output_folder.mkdirs())
-				context.raiseEvent(MessageEventType.create(String.format("Unable to create output folder (%s). Results will not be stored.", output_folder_path)));
+				{
+				final MuseEvent event = MessageEventType.create(String.format("Unable to create output folder (%s). Results will not be stored.", output_folder_path));
+				event.addTag(MuseEvent.ERROR);
+				context.raiseEvent(event);
+				_output_folder = null;
+				}
+		if (_output_folder != null)
+			context.raiseEvent(MessageEventType.create(String.format("Will store results locally at %s.", _output_folder.getAbsolutePath())));
 		}
 
 	@Override
