3b11cdf806b21de73125c846ca267f0966120373
chris <chris@ide4selenium.com>
2018-11-28 19:02:09 -0500

    move to Java 10 Service API

--- commandline/src/main/java/org/musetest/commandline/Launcher.java
+++ commandline/src/main/java/org/musetest/commandline/Launcher.java
@@ -4,7 +4,6 @@ import io.airlift.airline.*;
 import org.musetest.core.commandline.*;
 import org.reflections.*;
 
-import javax.imageio.spi.*;
 import java.util.*;
 
 /**
@@ -15,11 +14,11 @@ public class Launcher
     @SuppressWarnings("unchecked")
     public static void main(String[] args)
         {
-        // dynamically lookup the commands using Java's ServiceRegistry. This looks at the META-INF/service files in jars on the classpath.
-        Iterator<MuseCommand> commands = ServiceRegistry.lookupProviders(MuseCommand.class);
+        // dynamically lookup the commands using Java's ServiceLoader. This looks at the META-INF/service files in jars on the classpath.
+        ServiceLoader<MuseCommand> loader = ServiceLoader.load(MuseCommand.class);
         List<Class<? extends Runnable>> implementors = new ArrayList<>();
-        while (commands.hasNext())
-            implementors.add((commands.next().getClass()));
+        for (MuseCommand command : loader)
+            implementors.add((command.getClass()));
 
         Cli.CliBuilder<Runnable> builder = Cli.<Runnable>builder("muse")
             .withDescription("Muse command-line tools")
