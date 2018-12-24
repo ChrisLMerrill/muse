a8ee7f780977d0895a9b1222cd376b35a3b0fb27
chris <chris@ide4selenium.com>
2018-12-23 21:10:41 -0500

    ExecuteJavascript (Selenium) step now has an 'Arguments' parameter for passing arguments into the script (such as an element to operate on).

--- selenium/src/main/java/org/musetest/selenium/steps/ExecuteJavascript.java
+++ selenium/src/main/java/org/musetest/selenium/steps/ExecuteJavascript.java
@@ -2,12 +2,15 @@ package org.musetest.selenium.steps;
 
 import org.musetest.core.*;
 import org.musetest.core.context.*;
+import org.musetest.core.events.*;
 import org.musetest.core.resource.*;
 import org.musetest.core.step.*;
 import org.musetest.core.step.descriptor.*;
 import org.musetest.core.steptest.*;
 import org.musetest.core.values.descriptor.*;
 import org.openqa.selenium.*;
+
+import java.util.*;
 
 /**
  * @author Christopher L Merrill (see LICENSE.txt for license details)
@@ -20,6 +23,7 @@ import org.openqa.selenium.*;
 @MuseStepShortDescription("Run a script in the browser")
 @MuseStepLongDescription("Resolve the script source to a string. Inject that into the browser and run it.")
 @MuseSubsourceDescriptor(displayName = "Script", description = "The script to execute (as a text string)", type = SubsourceDescriptor.Type.Named, name = ExecuteJavascript.SCRIPT_PARAM)
+@MuseSubsourceDescriptor(displayName = "Arguments", description = "Arguments to pass into the script (accessed as arguments[N])", type = SubsourceDescriptor.Type.List, name = ExecuteJavascript.ARGUMENTS_PARAM)
 public class ExecuteJavascript extends BrowserStep
     {
     @SuppressWarnings("unused") // called via reflection
@@ -27,12 +31,22 @@ public class ExecuteJavascript extends BrowserStep
         {
         super(config);
         _script = getValueSource(config, SCRIPT_PARAM, true, project);
+        _arguments = getValueSource(config, ARGUMENTS_PARAM, false, project);
         }
 
     @Override
     public StepExecutionResult executeImplementation(StepExecutionContext context) throws MuseExecutionError
         {
         String script = getValue(_script, context, false, String.class);
+        Object[] arg_list = null;
+        if (_arguments != null)
+            {
+            Object args = getValue(_arguments, context, true, Object.class);
+            if (args instanceof List)
+                arg_list = ((List) args).toArray(new Object[0]);
+            else
+                return new BasicStepExecutionResult(StepExecutionStatus.ERROR, "The 'arguments' parameter must be a list. Instead, it was a " + args.getClass().getSimpleName());
+            }
 
         WebDriver driver = getDriver(context);
         if (driver instanceof JavascriptExecutor)
@@ -40,7 +54,7 @@ public class ExecuteJavascript extends BrowserStep
             Object result;
             try
                 {
-                result = ((JavascriptExecutor) driver).executeScript(script);
+                result = ((JavascriptExecutor) driver).executeScript(script, arg_list);
                 }
             catch (Exception e)
                 {
@@ -59,8 +73,10 @@ public class ExecuteJavascript extends BrowserStep
         }
 
     private MuseValueSource _script;
+    private MuseValueSource _arguments;
 
     public final static String SCRIPT_PARAM = "script";
+    public final static String ARGUMENTS_PARAM = "arguments";
 
     public final static String TYPE_ID = ExecuteJavascript.class.getAnnotation(MuseTypeId.class).value();
     }
