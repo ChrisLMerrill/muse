72933d9f3c63df1364978dc6401539f718e524e4
chris <chris@ide4selenium.com>
2019-07-07 21:30:02 -0400

    prune dead code

--- seleniumide/src/main/java/org/musetest/seleniumide/conditions/ElementNotPresentConverter.java
+++ seleniumide/src/main/java/org/musetest/seleniumide/conditions/ElementNotPresentConverter.java
@@ -2,9 +2,7 @@ package org.musetest.seleniumide.conditions;
 
 import org.musetest.builtins.value.logic.*;
 import org.musetest.core.values.*;
-import org.musetest.selenium.conditions.*;
 import org.musetest.seleniumide.*;
-import org.musetest.seleniumide.locators.*;
 
 /**
  * @author Christopher L Merrill (see LICENSE.txt for license details)
